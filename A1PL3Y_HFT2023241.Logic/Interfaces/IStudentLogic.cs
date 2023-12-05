d253c31e0d25f3d62b88e66cee238a31fc0866aa
norbert99@stud.uni-obuda.hu <norbert99@stud.uni-obuda.hu>
2023-12-05 11:00:20 +0100

    Crud methods created

--- A1PL3Y_HFT2023241.Logic/Interfaces/IStudentLogic.cs
+++ A1PL3Y_HFT2023241.Logic/Interfaces/IStudentLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using A1PL3Y_HFT2023241.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace A1PL3Y_HFT2023241.Logic.Interfaces
 {
-    internal interface IStudentLogic
+    public interface IStudentLogic
     {
+        void Create(StudentModel item);
+        StudentModel Read(int id);
+        IQueryable<StudentModel> ReadAll();
+        void Update(StudentModel item);
+        void Delete(int id);
     }
 }
