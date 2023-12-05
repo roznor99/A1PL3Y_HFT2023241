b05c63527a05aea94a4dbb72b20c40d1a69a2775
norbert99@stud.uni-obuda.hu <norbert99@stud.uni-obuda.hu>
2023-12-05 17:08:03 +0100

    StudentController implemented

--- A1PL3Y_HFT2023241.Endpoint/Controllers/StudentController.cs
+++ A1PL3Y_HFT2023241.Endpoint/Controllers/StudentController.cs
@@ -1,6 +1,49 @@
-﻿namespace A1PL3Y_HFT2023241.Endpoint.Controllers
+﻿using A1PL3Y_HFT2023241.Logic.Interfaces;
+using A1PL3Y_HFT2023241.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace A1PL3Y_HFT2023241.Endpoint.Controllers
 {
-    public class StudentController
+    [Route("[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
     {
+        IStudentLogic logic;
+
+        public StudentController(IStudentLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<StudentModel> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+        public StudentModel Read(int id)
+        {
+            return this.logic.Read(id);
+        }
+
+        [HttpPost]
+        public void Create([FromBody] StudentModel value)
+        {
+            this.logic.Create(value);
+        }
+
+        [HttpPut]
+        public void Update([FromBody] StudentModel value)
+        {
+            this.logic.Update(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
+        }
     }
 }
