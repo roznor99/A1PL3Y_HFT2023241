cac51f63d49eded47f1324aa08853bede2a39616
norbert99@stud.uni-obuda.hu <norbert99@stud.uni-obuda.hu>
2023-12-05 17:03:58 +0100

    EnrollmentController implemented

--- A1PL3Y_HFT2023241.Endpoint/Controllers/EnrollmentController.cs
+++ A1PL3Y_HFT2023241.Endpoint/Controllers/EnrollmentController.cs
@@ -1,6 +1,49 @@
-﻿namespace A1PL3Y_HFT2023241.Endpoint.Controllers
+﻿using A1PL3Y_HFT2023241.Logic.Interfaces;
+using A1PL3Y_HFT2023241.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace A1PL3Y_HFT2023241.Endpoint.Controllers
 {
-    public class EnrollmentController
+    [Route("[controller]")]
+    [ApiController]
+    public class EnrollmentController : ControllerBase
     {
+        IEnrollmentLogic logic;
+
+        public EnrollmentController(IEnrollmentLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<EnrollmentModel> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+        public EnrollmentModel Read(int id)
+        {
+            return this.logic.Read(id);
+        }
+
+        [HttpPost]
+        public void Create([FromBody] EnrollmentModel value)
+        {
+            this.logic.Create(value);
+        }
+
+        [HttpPut]
+        public void Update([FromBody] EnrollmentModel value)
+        {
+            this.logic.Update(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
+        }
     }
 }
