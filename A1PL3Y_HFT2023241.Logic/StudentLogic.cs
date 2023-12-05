ec4c63b72dcce0982b9e5bc30cdd1263d0254615
norbert99@stud.uni-obuda.hu <norbert99@stud.uni-obuda.hu>
2023-12-05 11:17:12 +0100

    StudentLogic class Created

--- A1PL3Y_HFT2023241.Logic/StudentLogic.cs
+++ A1PL3Y_HFT2023241.Logic/StudentLogic.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using A1PL3Y_HFT2023241.Logic.Interfaces;
+using A1PL3Y_HFT2023241.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,31 @@ using System.Threading.Tasks;
 
 namespace A1PL3Y_HFT2023241.Logic
 {
-    internal class StudentLogic
+    public class StudentLogic : IStudentLogic
     {
+        public void Create(StudentModel item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public StudentModel Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<StudentModel> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(StudentModel item)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
