af547f4fc95077558785d27d36a09c4dec3750a4
norbert99@stud.uni-obuda.hu <norbert99@stud.uni-obuda.hu>
2023-12-05 17:02:15 +0100

    CourStatController implemented

--- A1PL3Y_HFT2023241.Endpoint/Controllers/CourStatController.cs
+++ A1PL3Y_HFT2023241.Endpoint/Controllers/CourStatController.cs
@@ -1,6 +1,25 @@
-﻿namespace A1PL3Y_HFT2023241.Endpoint.Controllers
+﻿using A1PL3Y_HFT2023241.Logic.Interfaces;
+using A1PL3Y_HFT2023241.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace A1PL3Y_HFT2023241.Endpoint.Controllers
 {
-    public class CourStatController
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class CourStatController : ControllerBase
     {
+        ICourseLogic logic;
+
+        public CourStatController(ICourseLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<CourseModel.HowManyInfo> CreditValuePerSubjects()
+        {
+            return this.logic.CreditValuePerSubjects();
+        }
     }
 }
