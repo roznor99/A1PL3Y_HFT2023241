536947eace770f76dda6ced10993581b2328c129
norbert99@stud.uni-obuda.hu <norbert99@stud.uni-obuda.hu>
2023-12-05 10:36:35 +0100

    Implementing Repository methods

--- A1PL3Y_HFT2023241.Repository/ModelReps/CourseRepository.cs
+++ A1PL3Y_HFT2023241.Repository/ModelReps/CourseRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using A1PL3Y_HFT2023241.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,16 @@ using System.Threading.Tasks;
 
 namespace A1PL3Y_HFT2023241.Repository.ModelReps
 {
-    internal class CourseRepository
+    public class CourseRepository : Repository<CourseModel>, IRepository<CourseModel>
     {
+        public override CourseModel Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(CourseModel item)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
