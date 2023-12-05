893d0689be0f3a6cc37845e64bc7dd6501828e72
norbert99@stud.uni-obuda.hu <norbert99@stud.uni-obuda.hu>
2023-12-05 12:51:06 +0100

    CRUD methods done

--- A1PL3Y_HFT2023241.Logic/EnrollmentLogic.cs
+++ A1PL3Y_HFT2023241.Logic/EnrollmentLogic.cs
@@ -5,32 +5,73 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static A1PL3Y_HFT2023241.Models.EnrollmentModel;
 
 namespace A1PL3Y_HFT2023241.Logic
 {
     public class EnrollmentLogic : IEnrollmentLogic
     {
+        A1PL3Y_HFT2023241.Repository.IRepository<EnrollmentModel> repo;
+
+        public EnrollmentLogic(Repository.IRepository<EnrollmentModel> repo)
+        {
+            this.repo = repo;
+        }
         public void Create(EnrollmentModel item)
         {
-            throw new NotImplementedException();
+            if (item.Grade == 0)
+            {
+                throw new ArgumentException("Grade cannot be Zero");
+            }
+            else
+            {
+                this.repo.Create(item);
+            }
         }
-
-        public void Delete(int id)
-        {
-            throw new NotImplementedException();
-        }
-
         public EnrollmentModel Read(int id)
         {
-            throw new NotImplementedException();
+            var enroll = this.repo.Read(id);
+            if (enroll == null)
+            {
+                throw new ArgumentException("Enrollment not exists");
+            }
+            return enroll;
+        }
+        public IQueryable<EnrollmentModel> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+        public void Update(EnrollmentModel item)
+        {
+            this.repo.Update(item);
+        }
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
         }
 
-        public IQueryable<EnrollmentModel> ReadAll()
+        public IEnumerable<EnrollmentModel.StudentsInfo> GoodStudentToBadStudent()
         {
             throw new NotImplementedException();
         }
 
-        public void Update(EnrollmentModel item)
+       
+
+      
+
+        public IEnumerable<EnrollmentModel.SubjectInfo> SubjectCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<EnrollmentModel.TitleInfo> SubjectPassAVG()
+        {
+            throw new NotImplementedException();
+        }
+
+        
+
+        public IEnumerable<EnrollmentModel.YearInfo> YearAVGs()
         {
             throw new NotImplementedException();
         }
