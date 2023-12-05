6e368f50def41ce8a5d63c65b8fe723119e4e825
norbert99@stud.uni-obuda.hu <norbert99@stud.uni-obuda.hu>
2023-12-05 17:05:44 +0100

    EnrollStatController implemented

--- A1PL3Y_HFT2023241.Endpoint/Controllers/EnrollStatController.cs
+++ A1PL3Y_HFT2023241.Endpoint/Controllers/EnrollStatController.cs
@@ -1,6 +1,43 @@
-﻿namespace A1PL3Y_HFT2023241.Endpoint.Controllers
+﻿using A1PL3Y_HFT2023241.Logic.Interfaces;
+using A1PL3Y_HFT2023241.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace A1PL3Y_HFT2023241.Endpoint.Controllers
 {
-    public class EnrollStatController
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class EnrollStatController : ControllerBase
     {
+        IEnrollmentLogic logic;
+
+        public EnrollStatController(IEnrollmentLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<EnrollmentModel.StudentsInfo> GoodStudentToBadStudent()
+        {
+            return this.logic.GoodStudentToBadStudent();
+        }
+
+        [HttpGet]
+        public IEnumerable<EnrollmentModel.TitleInfo> SubjectPassAVG()
+        {
+            return this.logic.SubjectPassAVG();
+        }
+
+        [HttpGet]
+        public IEnumerable<EnrollmentModel.SubjectInfo> SubjectCount()
+        {
+            return this.logic.SubjectCount();
+        }
+
+        [HttpGet]
+        public IEnumerable<EnrollmentModel.YearInfo> YearAVGs()
+        {
+            return this.logic.YearAVGs();
+        }
     }
 }
