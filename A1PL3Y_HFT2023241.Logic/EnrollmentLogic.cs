e33bf608ab5f2cb2e0723f3d8518d0592f107637
norbert99@stud.uni-obuda.hu <norbert99@stud.uni-obuda.hu>
2023-12-05 11:15:06 +0100

    Logic classes created

--- A1PL3Y_HFT2023241.Logic/EnrollmentLogic.cs
+++ A1PL3Y_HFT2023241.Logic/EnrollmentLogic.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using A1PL3Y_HFT2023241.Logic.Interfaces;
+using A1PL3Y_HFT2023241.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,31 @@ using System.Threading.Tasks;
 
 namespace A1PL3Y_HFT2023241.Logic
 {
-    internal class EnrollmentLogic
+    public class EnrollmentLogic : IEnrollmentLogic
     {
+        public void Create(EnrollmentModel item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EnrollmentModel Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<EnrollmentModel> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(EnrollmentModel item)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
