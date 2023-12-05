c9da2659a529f493e18c0c7d77ba225fccd41279
norbert99@stud.uni-obuda.hu <norbert99@stud.uni-obuda.hu>
2023-12-05 10:41:46 +0100

    Implement Read method

--- A1PL3Y_HFT2023241.Repository/ModelReps/EnrollmentRepository.cs
+++ A1PL3Y_HFT2023241.Repository/ModelReps/EnrollmentRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using A1PL3Y_HFT2023241.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace A1PL3Y_HFT2023241.Repository.ModelReps
 {
-    internal class EnrollmentRepository
+    public class EnrollmentRepository : Repository<EnrollmentModel>, IRepository<EnrollmentModel>
     {
+        public override EnrollmentModel Read(int id)
+        {
+            return ctx.EnrollmentModels.FirstOrDefault(t => t.EnrollmentID == id);
+        }
+
+       
     }
 }
