f99aec3edf28ae88c8adac0601b9ce25aa5816ed
norbert99@stud.uni-obuda.hu <norbert99@stud.uni-obuda.hu>
2023-12-05 16:59:48 +0100

    CourseController

--- A1PL3Y_HFT2023241.Endpoint/Controllers/CourseController.cs
+++ A1PL3Y_HFT2023241.Endpoint/Controllers/CourseController.cs
@@ -1,6 +1,49 @@
-﻿namespace A1PL3Y_HFT2023241.Endpoint.Controllers
+﻿using A1PL3Y_HFT2023241.Logic.Interfaces;
+using A1PL3Y_HFT2023241.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace A1PL3Y_HFT2023241.Endpoint.Controllers
 {
-    public class CourseController
+    [Route("[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
     {
+        ICourseLogic logic;
+
+        public CourseController(ICourseLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<CourseModel> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+        public CourseModel Read(int id)
+        {
+            return this.logic.Read(id);
+        }
+
+        [HttpPost]
+        public void Create([FromBody] CourseModel value)
+        {
+            this.logic.Create(value);
+        }
+
+        [HttpPut]
+        public void Update([FromBody] CourseModel value)
+        {
+            this.logic.Update(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
+        }
     }
 }
